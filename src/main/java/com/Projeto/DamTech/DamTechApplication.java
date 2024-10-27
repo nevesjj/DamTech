103f53d5518a58a76056f56ea83798984595046e
Joao Neves <joaovictorneves28@hotmail.com.br>
2024-10-26 21:17:35 -0300

    Iniciando Back-end da DamTech

---
+++ src/main/java/com/Projeto/DamTech/DamTechApplication.java
@@ -0,0 +1,13 @@
+package com.Projeto.DamTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Projeto/DamTech/DamTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projeto.DamTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
