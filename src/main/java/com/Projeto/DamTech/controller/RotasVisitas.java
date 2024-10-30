2c4a946a1fbe1f92b39dd9d2f96253d6436752ec
Joao Neves <joaovictorneves28@hotmail.com.br>
2024-10-29 23:05:16 -0300

    Criacao de rotas Post e Get para o banco de Visitas !!!

---
+++ src/main/java/com/Projeto/DamTech/controller/RotasVisitas.java
@@ -0,0 +1,30 @@
+package com.Projeto.DamTech.controller;
+
+import com.Projeto.DamTech.entity.Visitas;
+import com.Projeto.DamTech.service.ServicosVisitas;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("v1/visitas")
+@CrossOrigin(origins = "http://localhost:4200/")
+public class RotasVisitas {
+
+    private final ServicosVisitas servicosVisitas;
+
+    @PostMapping
+    public ResponseEntity<Visitas> criarVisita(@RequestBody Visitas visita) {
+        Visitas salvarVisitas = servicosVisitas.criarVisita(visita);
+        return ResponseEntity.status(201).body(salvarVisitas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Visitas> obterVisita(@PathVariable Long id) {
+        Optional<Visitas> visitas = servicosVisitas.buscarVisita(id);
+        return ResponseEntity.status(200).body(visitas.get());
+    }
+}
---
+++ src/main/java/com/Projeto/DamTech/service/ServicosVisitas.java
@@ -0,0 +1,21 @@
+package com.Projeto.DamTech.service;
+
+import com.Projeto.DamTech.entity.Visitas;
+import com.Projeto.DamTech.repository.RepoVisita;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ServicosVisitas {
+
+    @Autowired
+    private RepoVisita repoVisita;
+
+    @Transactional
+    public Visitas criarVisita(Visitas visita) {return repoVisita.save(visita);}
+
+    public Optional<Visitas> buscarVisita(Long id) {return repoVisita.findById(id);}
+}
